89e6a9f57e7a1c16f12d8b45abea93c326d03771
Rob Calcote <robertc@qualtrics.com>
2021-10-17 18:21:13 -0600

    Practiced Route Parameter Constraints

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,34 @@ Route::get('/contact', function() {
     return 'Contact';
 })->name('home.contact');
 
+// example route to show the power of parameters (optional as well with ? + default)
+Route::get('/month/{id?}', function($id = '5') {
+    return 'Your month id is: ' . $id;
+})
+// ->where([
+//     'id' => '[0-9]+'
+// ])
+->name('home.month.monthId');
+
+
 // NOTES:
 // ->name('example.example'); ---> This gives the Route a specific name
 // you can list these names out from the terminal with ```php artisan route:list```
 // It is "best practice" according to Piotr to always give your routes names
+// You can write Regex constraints... 
+// which are basically just validations on the route prior to hitting the controller... good security feature
+// These are done with the ->where() if you want them to be Route specific... you can also do this in:
+// RouteServiceProvider.php
+
+// TODO:
+// Go through and write routes for all the basic functionality that I will want to do.
+// Be sure to include defaults if you want to make optional paramters
+// practice Regex writing
+// Add as many validations as make sense
+// Be sure to define a naming convention for your routes and be sure to name all Routes.
+// Pages to view...
+//      
+//      
+// API Routes to define
+//      
+//      
