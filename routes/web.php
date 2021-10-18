d66ffcd34047d4e97000096af4b05c61259ff8a0
Rob Calcote <robertc@qualtrics.com>
2021-10-17 18:03:07 -0600

    Added a contact route, added a few notes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return 'Home Page';
+})->name('home.index');
+
+// example route that we added as part of the tutorial
+Route::get('/contact', function() {
+    return 'Contact';
+})->name('home.contact');
+
+// NOTES:
+// ->name('example.example'); ---> This gives the Route a specific name
+// you can list these names out from the terminal with ```php artisan route:list```
+// It is "best practice" according to Piotr to always give your routes names
