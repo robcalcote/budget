1f509f134bf497bab5d7e7c53d96f26e1bd5d3d9
Rob Calcote <robertc@qualtrics.com>
2021-10-19 22:49:08 -0600

    renamed Routes and made code more consistent

--- routes/web.php
+++ routes/web.php
@@ -13,42 +13,41 @@ use Illuminate\Support\Facades\Route;
 
 // ROUTE:  home
 // DESC:   returns the main page of the application
-Route::get('/', function () {
-    return view('welcome.blade.php', ['name' => 'Rob']);
-})->name('home.index');
+Route::view('/', 'home',  ['name' => 'Rob']
+)->name('view.home');
 
 // ROUTE:  category
 // METHOD: get, post
 // PARAMS: id (optional)
 Route::match(['get', 'post'], '/category/{id?}', function ($id=null) {
-    return 'get, post 1 category... ' + $id;
-})->name('single.category');
+    return 'get, post 1 category... ' . $id;
+})->name('api.category');
 
 // ROUTE:  categories
 // METHOD: get
 Route::get('/categories', function () {
     return 'get all categories... ';
-})->name('multiple.categories');
+})->name('api.categories');
 
 // ROUTE:  month
 // METHOD: get, post
 // PARAMS: id (optional)
 Route::match(['get', 'post'], '/month/{id?}', function ($id=null) {
-    return 'get, post 1 month... ' + $id;
-})->name('single.month');
+    return 'get, post 1 month... ' . $id;
+})->name('api.month');
 
 // ROUTE:  months
 // METHOD: get
 Route::get('/months', function () {
     return 'get all months... ';
-})->name('multiple.months');
+})->name('api.months');
 
 // ROUTE:  transaction
 // METHOD: get, post
 // PARAMS: id (optional)
 Route::match(['get', 'post'], '/transaction/{id?}', function ($id=null) {
-    return 'get, post 1 transaction... ' + $id;
-})->name('single.transaction');
+    return 'get, post 1 transaction... ' . $id;
+})->name('api.transaction');
 
 // ROUTE:  transactions
 // METHOD: get
@@ -56,4 +55,4 @@ Route::match(['get', 'post'], '/transaction/{id?}', function ($id=null) {
 //         categoryid (optional)
 Route::get('/transactions', function() {
     return 'get all transactions... ';
-})->name('multiple.transactions');
+})->name('api.transactions');
